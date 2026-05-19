67339b7e1e2d8626846adc665c290e5326c1eae5
ILBBS <iovanny1213@gmail.com>
2026-05-19 01:28:46 -0500

    Creación de clase Main

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,5 +1,9 @@
 package App;
 
-public class Main {
+import vista.FrmInventario;
 
+public class Main {
+    public static void main(String[] args) {
+        new FrmInventario().setVisible(true);
+    }
 }
